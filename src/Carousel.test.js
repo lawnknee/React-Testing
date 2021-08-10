90fd9f0b08e2960002b15c154d3cdd63d8e78113
Loni <kuangloni@gmail.com>
2021-08-10 15:59:12 -0700

    smoke and snapshots added to Card and Carousel tests. Left arrow bug fixed

--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -2,6 +2,15 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import Carousel from "./Carousel";
 
+it("renders without crashing", function() {
+  render(<Carousel />);
+})
+ 
+it("matches snapshot", function() {
+  const { container } = render(<Carousel />);
+  expect(container).toMatchSnapshot();
+})
+
 it("works when you click on the right arrow", function() {
   const { container } = render(<Carousel />);
 
@@ -23,5 +32,33 @@ it("works when you click on the right arrow", function() {
   ).not.toBeInTheDocument();
   expect(
     container.querySelector('img[alt="Photo by Pratik Patel on Unsplash"]')
+  ).toBeInTheDocument();
+});
+
+it("works when you click on the left arrow", function() {
+  const { container } = render(<Carousel />);
+
+  // move forward in the carousel
+  const rightArrow = container.querySelector(".fa-chevron-circle-right");
+  fireEvent.click(rightArrow);
+  
+  // expect the second image to show, but not the first
+  expect(
+    container.querySelector('img[alt="Photo by Pratik Patel on Unsplash"]')
+  ).toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="Photo by Richard Pasquarella on Unsplash"]')
+  ).not.toBeInTheDocument();
+
+  // move backward in the carousel
+  const leftArrow = container.querySelector(".fa-chevron-circle-left");
+  fireEvent.click(leftArrow);
+
+  // expect the first image to show, but not the second
+  expect(
+    container.querySelector('img[alt="Photo by Pratik Patel on Unsplash"]')
+  ).not.toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="Photo by Richard Pasquarella on Unsplash"]')
   ).toBeInTheDocument();
 });
